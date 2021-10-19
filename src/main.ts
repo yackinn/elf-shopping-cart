cb5eb595fc6cd7f31878dba6ab5537e5ba66611f
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-10-19 19:14:23 +0300

    fix devtools

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import './devtools';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { actionsDispatcher } from '@ngneat/effects';
+import { devTools } from '@ngneat/elf-devtools';
 
 if (environment.production) {
   enableProdMode();
+} else {
+  devTools({
+    actionsDispatcher: actionsDispatcher
+  });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
